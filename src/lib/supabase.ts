1d3e06cfe173622cc5c076f5eec1801cb97752e4
roshankodi <roshankodi019@gmail.com>
2026-05-25 12:00:46 +0530

    fix supabase websocket typing

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,6 @@
-"server-only";
-
 import { createClient } from "@supabase/supabase-js";
-import ws from "ws";
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  {
-    realtime: {
-      transport: ws,
-    },
-  }
+  process.env.SUPABASE_ANON_KEY!
 );
